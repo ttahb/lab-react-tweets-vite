ef8921f772c02ea60054641ce3fe8704e02f488e
ttahb <bhattvijay69@hotmail.com>
2024-02-03 23:13:26 +0530

    multiple tweets mapped

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,12 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet tweet={tweetsArray[0]}/>
+      {/* <Tweet tweet={tweetsArray[0]}/> */}
+      {
+        tweetsArray.map((tweet, index)=>{
+          return <Tweet key={index} tweet={tweet}/>
+        })
+      }
     </div>
   );
 }
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,4 +1,8 @@
 import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Actions from "./Actions";
 
 function Tweet(props) {
   //console.log('props', props);
@@ -15,25 +19,12 @@ function Tweet(props) {
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">{user.name}</span>
-            <span className="handle">@{user.handle}</span>
-          </span>
-
-          <span className="timestamp">{tweet.timestamp}</span>
+          <User name={user.name} handle={user.handle} />
+          <Timestamp timestamp={tweet.timestamp}/>
         </div>
 
-        <p className="message">
-          {tweet.message} 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
+        <Message message={tweet.message}/>
+        <Actions/>
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
